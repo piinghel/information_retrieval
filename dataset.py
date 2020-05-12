ef830dcd42bb716b55d982e1e9919c1f9f2d38c1
kriekemanssiegfried <siegfried.kriekemans@student.kuleuven.be>
2020-05-12 19:01:00 +0200

    added import from old project

--- dataset.py
+++ dataset.py
@@ -1,20 +1,29 @@
+import json
+
 import torch
+import numpy as np
 from torch.utils.data import Dataset
+
+from preprocessing import read_split_images
+
+PATH = "input/"
+
 
 class FLICKR30K(Dataset):
     def __init__(self, mode):
         super().__init__()
         assert mode in ['train', 'val', 'test']
         self.mode = mode
+        self.internal_set_images = read_split_images(path=PATH, verbose=True, mode=self.mode)
+        self.internal_set_captions = json.load(open('output/data/{}.json'.format(self.mode), 'r'))
+        captions_per_image = len(self.internal_set_captions) / len(self.internal_set_images)
+        self.internal_set_images = np.repeat(self.internal_set_images, repeats=captions_per_image, axis=0)
 
     def __getitem__(self, index):
-        # TODO: Overwrite the __getitem__ method of Dataset to return the data sample at the given index
-        return
+        return self.internal_set_images[index], self.internal_set_captions[index]
 
     def __len__(self):
-        # TODO: Overwrite the __len__ method of Dataset to return the size of the dataset
-        return
+        return len(self.internal_set_images)
 
     def get_dimensions(self):
-        # TODO: Implement this method to return the dimensions of the image and text features
-        return
+        return self.internal_set_images.shape, self.internal_set_captions.shape
